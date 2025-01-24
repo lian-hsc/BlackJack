2d7f7015b73fe07ce540ed621f758f36c5d2bffa
CheeseTastisch <lian.hoerschlaeger@gmail.com>
2025-01-24 14:02:31 +0100

    Allow anything to be colored

--- terminal/src/main/kotlin/me/blackjack/terminal/Color.kt
+++ terminal/src/main/kotlin/me/blackjack/terminal/Color.kt
@@ -2,14 +2,14 @@ package me.blackjack.terminal
 
 import me.blackjack.terminal.impl.constant.Ansi
 
-fun String.rgb(r: Int, g: Int, b: Int) =
+fun Any.rgb(r: Int, g: Int, b: Int) =
     "${Ansi.ESCAPE_SEQUENCE}${Ansi.Color.foreground(r, g, b)}$this${Ansi.ESCAPE_SEQUENCE}${Ansi.Color.RESET_FOREGROUND}"
 
-fun String.rgb(hex: String) =
+fun Any.rgb(hex: String) =
     "${Ansi.ESCAPE_SEQUENCE}${Ansi.Color.foreground(hex)}$this${Ansi.ESCAPE_SEQUENCE}${Ansi.Color.RESET_FOREGROUND}"
 
-fun String.bg(r: Int, g: Int, b: Int) =
+fun Any.bg(r: Int, g: Int, b: Int) =
     "${Ansi.ESCAPE_SEQUENCE}${Ansi.Color.background(r, g, b)}$this${Ansi.ESCAPE_SEQUENCE}${Ansi.Color.RESET_BACKGROUND}"
 
-fun String.bg(hex: String) =
+fun Any.bg(hex: String) =
     "${Ansi.ESCAPE_SEQUENCE}${Ansi.Color.background(hex)}$this${Ansi.ESCAPE_SEQUENCE}${Ansi.Color.RESET_BACKGROUND}"
